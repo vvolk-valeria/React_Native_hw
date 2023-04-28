de98f506fc86f3338c0ec51788d22903ed457943
Volkova Valeria <vvolk.valeria@gmail.com>
2023-04-28 17:45:32 +0300

    init

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, StyleSheet, Text, View } from 'react-native';
+import  LoginScreen  from "./Screens/LoginScreen";
+import  RegistrationScreen  from "./Screens/RegistrationScreen";
+import { useFonts } from 'expo-font';
+import React, { useState, useEffect } from "react";
+
+
+const windowDimensions = Dimensions.get('window');
+const screenDimensions = Dimensions.get('screen');
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'), //400
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'), //500
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'), //700
+    'ita': require('./assets/fonts/Roboto-ThinItalic.ttf'), //700
+  });
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  // const [dimensions, setDimensions] = useState({
+  //   window: windowDimensions,
+  //   screen: screenDimensions,
+  // });
+
+
+  // useEffect(() => {
+  //   const subscription = Dimensions.addEventListener(
+  //     'change',
+  //     ({window, screen}) => {
+  //       setDimensions({window, screen});
+  //     },
+  //   );
+  //   return () => subscription?.remove();
+  // });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    // <LoginScreen />
+
+    <RegistrationScreen/>
+    // <View style={styles.container} onLayout={onLayoutRootView}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+
+const initialState = {
+  avatar:'',
+  login:'',
+  email: "",
+  password: "",
+};
+
+export default function RegistrationScreen() {
+  const [formData, setFormData] = useState(initialState);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const KeyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const onLogin = () => {
+    KeyboardHide();
+    console.log(formData);
+    setFormData(initialState);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={KeyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.img}
+          // source={require("../img/bg.png")}
+        >
+          <KeyboardAvoidingView 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{
+        ...styles.form,
+        paddingBottom: isShowKeyboard ? 32 : 100,
+      }}
+        >
+
+<TextInput
+              value={formData.avatar}
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={(value) =>
+                setFormData((prevState) => ({ ...prevState, avatar: value }))
+              }
+              style={styles.photo}
+            />
+             <TouchableOpacity
+              style={styles.btn_add}
+              activeOpacity={0.7}
+            //  onPress={}
+            >
+              
+            </TouchableOpacity>
+
+
+            <Text style={styles.title}>Регистрация</Text>
+            <TextInput
+              value={formData.login}
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={(value) =>
+                setFormData((prevState) => ({ ...prevState, login: value }))
+              }
+              placeholder="Логин"
+              style={{ ...styles.input, marginBottom: 16 }}
+            />
+            <TextInput
+              value={formData.email}
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={(value) =>
+                setFormData((prevState) => ({ ...prevState, email: value }))
+              }
+              placeholder="Адрес электронной почты"
+              style={{ ...styles.input, marginBottom: 16 }}
+            />
+            <TextInput
+              value={formData.password}
+              onFocus={() => setIsShowKeyboard(true)}
+              onChangeText={(value) =>
+                setFormData((prevState) => ({ ...prevState, password: value }))
+              }
+              placeholder="Пароль"
+              secureTextEntry={true}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              style={styles.btn}
+              activeOpacity={0.7}
+              onPress={onLogin}
+            >
+              <Text style={styles.btnTitle}>Войти</Text>
+            </TouchableOpacity>
+            <Text style={styles.link}>Нет аккаунта? Зарегистрироваться</Text>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ecf0f1",
+  },
+  form: {
+    position:"relative",
+    paddingTop: 32,
+    paddingBottom: 100,
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 25,
+  },
+  img: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  photo:{
+    position:"absolute",
+    borderColor: "#E8E8E8",
+    backgroundColor:"#F6F6F6",
+    borderRadius: 8,
+    width:120,
+    height:120,
+  },
+  btn_add:{
+    width:25,
+    height:25,
+    borderColor: "#FF6C00",
+    backgroundColor:"#FFFFFF",
+    borderRadius: 50,
+  },
+  input: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    backgroundColor:"#F6F6F6",
+    borderRadius: 8,
+    marginHorizontal: 16,
+    color: "#212121",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+  },
+  btn: {
+    marginHorizontal: 16,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "transparent",
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginBottom: 16,
+    marginTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+  },
+  title: {
+    color: "#212121",
+    textAlign: "center",
+    fontSize: 30,
+    marginBottom: 32,
+    fontFamily: "Roboto-Medium",
+  },
+  link: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#1B4371",
+    fontFamily: "Roboto-Regular",
+  },
+});
